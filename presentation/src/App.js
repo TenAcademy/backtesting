bdfef15d9820534d5eec6fe66e6f1838f3113b88
natty <natnaelmasresha@gmail.com>
2022-10-14 11:16:29 +0000

    design the App component

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path={"/login"} exact element={<Login />} />
+
+        <Route exact path='/' element={<ProtectedRoute />}>
+          <Route exact path='/' element={<Home />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
